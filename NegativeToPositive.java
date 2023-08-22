e145183308985f7cda6d4a2c5903ca575cbff8cf
mercymagdalene2 <142736770+mercymagdalene2@users.noreply.github.com>
2023-08-22 21:17:28 +0530

    Add files via upload
    Assignments

---
+++ NegativeToPositive.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class NegativeToPositive {
+	public static void main(String[] args) {
+		int negno= -20;
+		int posno=0;
+		System.out.println("The negative number is :"+negno);
+		if (negno< 0) {
+			posno=negno *(-1);
+			System.out.println("The positive number is:" +posno);}
+		
+		
+		
+			
+		}
+		
+		
+	}
+	
+	
+
+
