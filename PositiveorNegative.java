170fbc1fa98fec1e1f96f223715c1274bf01454b
mercymagdalene2 <142736770+mercymagdalene2@users.noreply.github.com>
2023-08-22 21:34:28 +0530

    Add files via upload
    assignments

---
+++ PositiveorNegative.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class PositiveorNegative {
+	public static void main(String[] args) {
+		int n=20;
+		if (n>1) {
+			System.out.println("The number given is a positive number:");
+			
+		}
+		else if (n<1) {
+			System.out.println("The number given is a negative number:");
+			}
+		else {
+			System.out.println("The given number is neutral");
+			
+		}
+	}
+	
+	
+	
+
+}
